397f1844d5c12bf9dc1d3862076fb85f9945dc19
David Dinh <dmdinh22@gmail.com>
2018-12-18 23:49:41 -0800

    beam me up scotty

---
+++ WeatherApp/ViewControllers/WeatherTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherTableViewController.swift
+//  WeatherApp
+//
+//  Created by David D on 12/18/18.
+//  Copyright © 2018 David D. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
