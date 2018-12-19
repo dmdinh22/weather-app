b3a361fbda5d0ffa05b771daa1b029a49231d5e2
David Dinh <dmdinh22@gmail.com>
2018-12-18 23:43:35 -0800

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by David D on 12/18/18.
+//  Copyright © 2018 David D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
